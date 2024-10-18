cbcc4bb1f194f9ec0b4bd813a5bfe01d41f78fe3
dw <dw@konakona.moe>
2024-10-18 14:18:20 +0200

    refactor(entrypoint): cp instead of mv

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ -z "$(ls -A "/dw-ring/src/static")" ] && mv static-tmp/* /dw-ring/src/static && rm -rf static-tmp
-[ ! -f "/dw-ring/config/config.yml" ]   && mv /dw-ring/config.yml /dw-ring/config/config.yml
+[ -z "$(ls -A "/dw-ring/src/static")" ] && cp -r static-tmp/* /dw-ring/src/static
+[ ! -f "/dw-ring/config/config.yml" ]   && cp /dw-ring/config.yml /dw-ring/config/config.yml
 
 node src/app.js --docker
